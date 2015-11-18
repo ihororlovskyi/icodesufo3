5441ef44371c0dcd9b6b4c73cbda234bd7664e06
Igor Orlovskiy <i_orlovskiy@wargaming.net>
2015-11-18 16:51:32 +0200

    continue styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,5 @@
-$(document).ready(function(){
-    $(".dropdown-button").click(function() {
-        $(".dropdown-menu").toggleClass("show-menu");
-        $(".dropdown-menu > li").click(function(){
-            $(".dropdown-menu").removeClass("show-menu");
-        });
-        $(".dropdown-menu.dropdown-select > li").click(function() {
-            $(".dropdown-button").html($(this).html());
-        });
-    });
-});
-
+/**
+* selectBox
+* https://github.com/marcj/jquery-selectBox
+*/
+$('select').selectBox();
